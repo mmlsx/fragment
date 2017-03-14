67a92ae88d15c7e02d377d814731f3cb557e6036
mmlsx <sxlmm93@163.com>
2017-03-14 11:52:02 +0800

    curl操作
    php curl操作

--- php-curl.php
+++ php-curl.php
@@ -10,16 +10,16 @@ function curl_api_post_json($params, $api_url){
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_TIMEOUT, 30);
     curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'Content-Type: application/json',
-			'Content-Length: ' . strlen($params))
-		);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	'Content-Type: application/json',
+	'Content-Length: ' . strlen($params))
+    );
     $response = curl_exec($ch);
-		$curl_errors='';
+    $curl_errors='';
     if(curl_errno($ch)) {
       $curl_errors='CURL错误代码:'.curl_errno($ch).'. 错误内容:'.curl_error($ch);
     }
